6eca803540f9055df4be69f51d1167f75644ca2d
MaheswariDarish <maheswaridj1312@gmail.com>
2024-02-18 01:08:32 +0530

    Homepage icon change

--- lib/screens/SreenHome.dart
+++ lib/screens/SreenHome.dart
@@ -8,107 +8,125 @@ class ScreenHome extends StatefulWidget {
 }
 
 class _ScreenHomeState extends State<ScreenHome> {
-List<Icon> nameicon=[
-  Icon(Icons.category,color: Colors.white,size:30),
-  Icon(Icons.category,color: Colors.white,size:30),
-  Icon(Icons.category,color: Colors.white,size:30),
-  Icon(Icons.category,color: Colors.white,size:30),
-];
-List names=[
-  "Public Notice",
-  "Students Notice",
-  "Tenders",
-  "Admissions"
-];
+  List<Icon> nameicon = [
+    Icon(Icons.info_outline, color: Colors.white, size: 30),
+    Icon(Icons.school, color: Colors.white, size: 30),
+    Icon(Icons.how_to_reg, color: Colors.white, size: 30),
+    Icon(Icons.work_outline, color: Colors.white, size: 30),
+  ];
+  List names = [
+    "Public Notice",
+    "Students Notice",
+    "Tenders",
+    "Admissions"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: [
           Container(
-            padding: EdgeInsets.only(top: 15,left: 15,right:15,bottom:10),
+            padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
             decoration: BoxDecoration(
-              color:Color.fromARGB(146, 173, 80, 8),
+              color: Color.fromARGB(146, 173, 80, 8),
               borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(20),
                 bottomRight: Radius.circular(20),
-              )
+              ),
             ),
             child: Column(
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Expanded(child: 
-                    Text('         HOME',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold)),
-                    
-                    
-            ),
-            IconButton(onPressed: (){}, icon: Icon(Icons.dehaze_outlined)),
+                    Expanded(
+                      child: Text(
+                        '         HOME',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.dehaze_outlined),
+                    ),
                   ],
                 )
               ],
             ),
           ),
           SizedBox(height: 20),
-          Container( 
-          child: CircleAvatar(
-            radius: 50, // This will be institute logo
-            backgroundColor: const Color.fromARGB(255, 79, 10, 10), 
-            
+          Container(
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: const Color.fromARGB(255, 79, 10, 10),
+            ),
           ),
-        ),
-         Text("Organisation",
-         textAlign: TextAlign.center,
-         style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold)
-         ),
-        SizedBox(height: 20),
-        Padding(padding: EdgeInsets.only(top: 20,left:15,right: 15),
-        child:Column(children: [
-          GridView.builder(
-            itemCount: names.length,
-            shrinkWrap:true,
-          physics:NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 1.1,
-            ), itemBuilder:(context,index){ return Column(
+          Text(
+            "Organisation",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 15, right: 15),
+            child: Column(
               children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical:5,horizontal: 15),
-                  height: 150,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color:Color.fromARGB(224, 53, 0, 0),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
-                      bottomLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    )
+                GridView.builder(
+                  itemCount: names.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1.1,
                   ),
-                  child: Center(
-                    child:Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                       children: [
-                        nameicon[index],
-                          SizedBox(height: 10),
-                          Text(
-                            names[index],
-                            style: TextStyle(color: Colors.white),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      height: 95,
+                      width: 250,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(224, 53, 0, 0),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Stack(
+                        children: [
+                          Center(
+                            child: nameicon[index],
                           ),
-                       ],)
-                  ),
-                )
+                          Positioned(
+                            bottom: 10,
+                            left: 0,
+                            right: 0,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                names[index],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ],
-            );})
-        ],))
-
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
