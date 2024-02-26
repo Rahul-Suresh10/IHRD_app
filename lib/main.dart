5db52abf46b3135f9cdc039ea5d0ab072fa95150
MaheswariDarish <maheswaridj1312@gmail.com>
2024-02-26 20:39:13 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/SreenHome.dart';
+import 'package:flutter_application_1/screens/StudentNotice.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
       ),
-      home: ScreenHome(),
+      home:PdfViewerSample(),
     );
   }
 }
--- lib/screens/StudentNotice.dart
+++ lib/screens/StudentNotice.dart
@@ -1,1 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+
+class PdfViewerSample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PDF Viewer Sample'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PdfViewerPage(),
+              ),
+            );
+          },
+          child: Text('Open PDF'),
+        ),
+      ),
+    );
+  }
+}
+
+class PdfViewerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PDF Viewer'),
+      ),
+      body: PDFView(
+        filePath: 'https://mrcet.com/downloads/digital_notes/CSE/II%20Year/DBMS.pdf', // Sample PDF URL
+      ),
+    );
+  }
+}
+
+
